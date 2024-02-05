283b88b540d63342de9b3a82a9c5d8068a1446da
RyanDude <guotajun@gmail.com>
2024-02-05 17:07:19 +0800

    add redis cache

--- core-service-sample/src/main/java/org/cloud/sample/Config/Config.java
+++ core-service-sample/src/main/java/org/cloud/sample/Config/Config.java
@@ -4,11 +4,13 @@ import feign.Capability;
 import feign.micrometer.MicrometerCapability;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableCaching
 public class Config {
     @Bean
     public RestTemplate getRestTemplate(RestTemplateBuilder builder)
